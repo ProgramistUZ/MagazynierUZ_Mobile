c7cb9cd496183389ebbb222a9fb6e68b893a9888
Dawid Naraziński <dawidelel@gmail.com>
2026-04-11 15:07:30 +0200

    Add bottom navigation bar to MainActivity

--- app/src/main/java/com/example/magazynieruz_mobile/MainActivity.java
+++ app/src/main/java/com/example/magazynieruz_mobile/MainActivity.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
         TextView textWelcome = findViewById(R.id.textWelcome);
         String username = getIntent().getStringExtra("username");
         textWelcome.setText(getString(R.string.welcome_message, username));
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.nav_home);
     }
 }
